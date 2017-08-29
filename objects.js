1a4c7687a8be60f60bb3dc5530790a8324d1eadf
Peter Kincaid <loaatao@gmail.com>
2017-08-29 15:58:21 -0500

    I don't watch game of thrones

--- objects.js
+++ objects.js
@@ -1,63 +1,94 @@
 // Create an object called user that has name, age, password properties and assign them appropriate values.
 
 // Code Here
-
+var user = {
+  name: 'Peter',
+  age: 23,
+  password: 'notgonna'
+};
 
 
 // Add an email and username, as well as a method called alertUser that alerts the users name, to your user object using dot notation.
 
 // Code Here
-
-
+user.email = 'welt@dodgeball.com';
+user.username = 'dodge';
+user.alertUser = function(){
+  alert(JSON.stringify(user));
+};
+// user.alertUser();
 
 // Now loop over your user object and alert each of the keys.
 
 // Code Here
-
+for(var key in user){
+  alert(key);
+}
 
 
 // Now loop over your user object and alert each of the values for each key.
 
 // Code Here
-
+for(var key in user){
+  alert(user[key]);
+// }
 
 
 // Remove the username from your user object
 
 // Code Here
-
+delete user.username;
 
 
 // Create an object called creditCard that has number, cvc, type, and limit properties and assign them appropriate values.
 
 // Code Here
-
+var creditCard = {
+  number: 123456789,
+  cvc: 126,
+  type: 'Visa',
+  limit: 9000
+};
 
 
 // Create a variable called alternating. Set it equal to the string bonusCategories.
 
 // Code Here
-
+var alternating = 'bonusCategories';
 
 
 /* Using bracket notation, add a bonusCategories key to your creditCard object using 'alternating'.
-    Set it's value to an array of 3 items that are objects. 
-    Each object should have one key value pair. 
+    Set it's value to an array of 3 items that are objects.
+    Each object should have one key value pair.
     That key value pair should be the category as the key and the percent it's work for the bonus as the value.
-    (e.g. Department Stores, 5%) 
-    
+    (e.g. Department Stores, 5%)
     Using dot notation add another key to your object called 'bank' with the value being an object with 2 keys,
         the bank name, and the bank address.
-    
     */
 
 // Code Here
+creditCard[alternating] = [
+  {
+    "First Category": '5%'
+  },
+  {
+    "Second Category": '6%'
+  },
+  {
+    "Third Category": '7%'
+  }
+];
+// console.log(creditCard, user);
 
 
 
 // Write a function that will take your creditCard object and user object as arguments and combine them into one object. /* HINT: You will need a for..in loop */
 
-// Code Here
+
+var newObj = Object.assign({},creditCard, user);
+
+console.log(newObj);
+
 
 
 /*Below you are given an object. Write a function that will loop over the allies property, remove LittleFinger, and insert Dragon Lady.
@@ -70,6 +101,20 @@ var obj = {
     alive: 'Sort of',
   };
 
-  // Code Here
 
-  
+function loopOver(obj){
+  var allies = obj.allies;
+
+  if(allies.indexOf('LittleFinger') === -1){
+    allies.push('Dragon Lady');
+    return allies;
+  }
+  for(var i = 0; i < allies.length; i++){
+    if(allies[i] === 'LittleFinger'){
+      allies.splice(i,1);
+    }
+  }
+  return allies;
+}
+
+loopOver(obj);
